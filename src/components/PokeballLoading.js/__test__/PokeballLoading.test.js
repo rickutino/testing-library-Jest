20b7b8716fc51e41b2de446de31e634ad8970464
rickutino <rickutino@gmail.com>
2021-06-06 19:32:50 +0900

    rendering components with default props

--- src/components/PokeballLoading.js/__test__/PokeballLoading.test.js
+++ src/components/PokeballLoading.js/__test__/PokeballLoading.test.js
@@ -3,4 +3,12 @@ import { render } from '@testing-library/react';
 import PokeballLoading from '../PokeballLoading';
 
 describe('PokeballLoading', () => {
+  test('should render with default props', () => {
+    const { container, getByAltText } = render(<PokeballLoading />);
+    const image = getByAltText(/Pokeball Loading/i);
+
+    expect(container).toBeInTheDocument();
+    expect(image).toBeInTheDocument();
+    expect(image.width).toBe(200);
+  });
 });
