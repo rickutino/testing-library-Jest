118f84b2623a72d79eb5114ba5350b8daa00212c
rickutino <rickutino@gmail.com>
2021-06-06 14:08:32 +0900

    rendering components image have correct widthe

--- src/components/EmptyResult/__test__/EmptyResult.test.js
+++ src/components/EmptyResult/__test__/EmptyResult.test.js
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import EmptyResult from '../EmptyResult';
 
 describe('EmptyResult', () => {
+
   test('should render with default props', () => {
     const { container, getByAltText, getByText } = render(<EmptyResult/>);
     const defaultMessage = 'Oops... Não encontramos nada.';
@@ -21,5 +22,13 @@ describe('EmptyResult', () => {
     const { getByText } = render(<EmptyResult message={newMessage}/>);
 
     expect(getByText(newMessage)).toBeInTheDocument();
+  });
+
+  test('image should have correct width', () => {
+    const newWidth = 300;
+    const { getByAltText } = render(<EmptyResult width={newWidth} />);
+    const image = getByAltText(/empty result/i);
+
+    expect(image.width).toBe(newWidth);
   });
 });
